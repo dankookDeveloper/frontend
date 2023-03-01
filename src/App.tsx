8b1ca862abc650482b3a6d098814f00814be63e2
bubbletea03 <bubbletea03@naver.com>
2023-03-01 16:47:01 +0900

    feat: 구조 설계

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import logo from "./logo.svg";
+import { Routes, Route } from "react-router-dom";
+import GlobalStyle from "./GlobalStyle";
+import { Main, ProfileEdit, Register } from "pages";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="*" element={<div> Not Found </div>} />
+        <Route path="/" element={<Main />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile-edit" element={<ProfileEdit />} />
+      </Routes>
+      <GlobalStyle />
+    </>
   );
 }
 
