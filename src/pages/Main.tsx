d961bbabc57104f4145b0211dfdc4ce298ee5d19
bubbletea03 <bubbletea03@naver.com>
2023-03-08 14:16:57 +0900

    feat: 배경 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,12 +3,18 @@ import styled from "styled-components";
 
 export default function MainPage() {
   return (
-    <Wrapper>
-      <Navbar />
-      <body>
-        <div>ㅇㅇ</div>
-      </body>
-    </Wrapper>
+    <>
+      <Background>
+        <GrayBox />
+        <TitleBox>D.velopers</TitleBox>
+      </Background>
+      <Wrapper>
+        <Navbar />
+        <section>
+          <div>ㅇㅇ</div>
+        </section>
+      </Wrapper>
+    </>
   );
 }
 
@@ -17,4 +23,25 @@ const Wrapper = styled.div`
   display: flex;
   width: 100%;
   height: 100%;
+`;
+
+const Background = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+`;
+
+const GrayBox = styled.div`
+  display: block;
+  position: static;
+  width: 100%;
+  height: 30%;
+  background-color: lightgray;
+`;
+
+const TitleBox = styled.div`
+  display: block;
+  position: static;
+  width: 100%;
+  height: 50px;
 `;
